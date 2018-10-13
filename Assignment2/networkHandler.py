53fe3f4409e236b9905e5750264fe5618876a487
youngapsmikes <michael.yifan.li@gmail.com>
2018-10-13 16:53:20 -0400

    Add files via upload

--- Assignment2/networkHandler.py
+++ Assignment2/networkHandler.py
@@ -4,7 +4,11 @@
 # networkHandler.py
 # Author: Quinn Donohue
 #-----------------------------------------------------------------------
-
+from sys import exit, argv, stderr
+from socket import socket
+from socket import AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+from pickle import dump
+from pickle import load
 from queryClass import Query
 
 #-----------------------------------------------------------------------
@@ -14,8 +18,8 @@ from queryClass import Query
 class NetworkHandler (object):
 
 	def __init__(self, ip, port):
-		self._ip = ip
-		self._port = port
+		self._ip = str(ip)
+		self._port = int(port)
 
 	def test(self, query):
 		query.printOut()
@@ -28,14 +32,42 @@ class NetworkHandler (object):
 		try:
 			sock = socket(AF_INET, SOCK_STREAM)
 			sock.connect((self._ip, self._port))
-			flo = sock.makefile(mode = 'w')
 			
+			# dumpy query object to regserver
+			outFlo = sock.makefile(mode = 'w')
+			queryClass = Query('cos', None , None, None, True)
+			dump(queryClass, outFlo)
+			outFlo.flush()
+
+			
+			# read in dictionary from regserver
+			inFlo = sock.makefile(mode = 'r')
+			dictionary = load(inFlo)
+
+			sock.close()
+			return dictionary
+
 		except:
 			print "oh no"
-
-
 	# Take in class ID, communicate classid to server, and recieve
 	# a dictionary in the same format as in assignment 1 that will be
 	# returned
-	def searchHandle(self, classid):
-		print "not done yet"
+	#def searchHandle(self, classid):
+
+
+def main(argv):
+
+    if len(argv) != 3:
+        print 'Usage: python %s host port' % argv[0]
+        exit(1)
+    nh = NetworkHandler(argv[1], argv[2])
+    q1 = nh.queryHandle("String")
+    print q1
+
+if __name__ == '__main__':
+	main(argv)
+
+
+
+
+#-----------------------------------------------------------------------
---
+++ Assignment2/regserver.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# courseserver3.py
+# Author: Quinn Donohue, Michael Li
+#-----------------------------------------------------------------------
+import sys
+from sys import exit, argv, stderr
+from socket import socket
+from socket import AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+from pickle import dump
+from pickle import load
+from threading import Thread 
+
+sys.path.append('C:\\COS333\\Assignment1\\')
+from queryClass import Query
+from SQLExecutor import executeSearch
+
+class ServerThread (Thread):
+
+    def __init__(self, sock, clientAddr):
+        Thread.__init__(self)
+        self._sock = sock
+        self._clientAddr = clientAddr
+        
+    def run(self):
+        print 'Spawned thread for ' + str(self._clientAddr)
+        
+        inFlo = self._sock.makefile(mode='r')
+        queryClass = load(inFlo)
+        
+        found = executeSearch(queryClass)
+
+        outFlo = self._sock.makefile(mode = 'w')
+        dump(found, outFlo)
+        outFlo.flush()
+
+        self._sock.close()
+        print 'Closed socket for ' + str(self._clientAddr)
+        print 'Exited thread for ' + str(self._clientAddr)
+
+def handleClient(sock, clientAddr): 
+
+    inFlo = sock.makefile(mode = 'r')
+    queryClass = load(inFlo)
+
+    outFlo = sock.makefile(mode = 'w')
+    dump(queryClass._dict, outFlo)
+    outFlo.flush()
+
+    print 'Wrote query to' + str(clientAddr)
+
+#-----------------------------------------------------------------------
+def main(argv):
+
+    BACKLOG = 5
+
+    if len(argv) != 2:
+        print 'Usage: python %s port' % argv[0]
+        exit(1)
+
+    try:
+        port = int(argv[1])
+
+        serverSock = socket(AF_INET, SOCK_STREAM)
+        print 'Opened server socket'
+        serverSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        serverSock.bind(('', port))
+        print 'Bound server socket to port'
+        serverSock.listen(BACKLOG)
+        print 'Listening'
+
+        while True:
+            sock, clientAddr = serverSock.accept()
+            print 'Accepted connection for ' + str(clientAddr)
+            print 'Opened socket for ' + str(clientAddr)
+            serverThread = ServerThread(sock, clientAddr);
+            serverThread.start();
+
+    except:
+        print "Oh no"
+# def _test(argv):
+
+#     BACKLOG = 5
+
+#     if len(argv) != 2:
+#         print 'Usage: python %s port' % argv[0]
+#         exit(1)
+
+#     try:
+#         port = int(argv[1])
+
+#         serverSock = socket(AF_INET, SOCK_STREAM)
+#         print 'Opened server socket'
+#         serverSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+#         serverSock.bind(('', port))
+#         print 'Bound server socket to port'
+#         serverSock.listen(BACKLOG)
+#         print 'Listening'
+
+#         while True:
+#             sock, clientAddr = serverSock.accept()
+#             print 'Accepted connection for ' + str(clientAddr)
+#             print 'Opened socket for ' + str(clientAddr)
+#             handleClient(sock, clientAddr)
+#             sock.close()
+#             print 'Closed socket for ' + str(clientAddr)
+            
+#     except Exception, e:
+#         print >>stderr, e
+
+if __name__ == '__main__':
+	main(argv)
